b3e7a4c2d7fab9baea9545351cc1010cf89522a8
Thomas Hammer <sparklez@detolly.no>
2018-02-11 09:31:32 +0100

    first commit

---
+++ Extensions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    static class Extensions
+    {
+        public static string repl(this string str, char one, char two)
+        {
+            int first = str.IndexOf(one);
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (i == first)
+                {
+                    builder.Append(two);
+                    continue;
+                }
+                builder.Append(str[i]);
+            }
+            return builder.ToString();
+        }
+        
+        public static string ReplaceFirst(this string text, string search, string replace)
+        {
+            int pos = text.IndexOf(search);
+            if (pos < 0)
+            {
+                return text;
+            }
+            return text.Substring(0, pos) + replace + text.Substring(pos + search.Length);
+        }
+
+        public static string inv(this string str)
+        {
+            var returnString = new StringBuilder();
+            for (int i = str.Length-1; i >= 0; i--)
+            {
+                returnString.Append(str[i]);
+            }
+            return returnString.ToString();
+        }
+
+        public static bool isDigit(this string str)
+        {
+            try {
+                int.Parse(str);
+            } catch {
+                return false;
+            }
+            return true;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Ledd
+    {
+        int random = new Random().Next(15);
+        public string text = "";
+
+        public Ledd(string text)
+        {
+            this.text = text;
+        }
+
+        public static Ledd Parse(string inp)
+        {
+            return new Ledd(inp);
+        }
+    }
+
+    class Program
+    {
+        string operators = "^/*+-";
+
+        static void Main(string[] args)
+        {
+            new Program().Run();
+        }
+
+        void Run()
+        {
+            string input;
+            while ((input = Console.ReadLine()) != "T")
+            {
+                input = input.Replace(" ", "");
+                Begin(input);
+            }
+        }
+
+        private string Begin(string input)
+        {
+            input = "(" + input + ")";
+            while (input.Contains("("))
+            {
+                string currentThing = "";
+                int last = input.IndexOf(')');
+                for (int i = last; i >= 0; i--)
+                {
+                    if (input[i] == '(')
+                    {
+                        currentThing = input.Substring(i + 1, last - i - 1);
+                        Console.WriteLine("Next step");
+                        Console.WriteLine(currentThing);
+                        break;
+                    }
+                }
+                string result = calc(currentThing);
+                input = input.ReplaceFirst("(" + currentThing + ")", result);
+                Console.WriteLine("Now we have:");
+                Console.WriteLine(input);
+            }
+            Console.WriteLine(input);
+            return input;
+        }
+
+        string calc(string input)
+        {
+            string newString = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                bool a = false;
+                if (operators.Contains(input[i]) && i > 0 && !operators.Contains(input[i - 1]))
+                {
+                    a = true;
+                    newString += " ";
+                }
+                newString += input[i];
+                newString += a ? " " : "";
+            }
+            var arr = new List<Ledd>();
+            var arr2 = newString.Split(' ');
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                arr.Add(Ledd.Parse(arr2[i]));
+            }
+            while (arr.Count > 1)
+                for (int j = 0; j < operators.Length; j++)
+                    for (int i = 0; i < arr.Count-1; i++)
+                    {
+                        if (arr[i].text == operators[j].ToString())
+                        {
+                            arr[i].text = operations(arr[i].text, arr[i - 1].text, arr[i + 1].text).ToString();
+                            arr.Remove(arr[i - 1]);
+                            i--;
+                            arr.Remove(arr[i+1]);
+                        }
+                    }
+            return arr[0].text;
+        }
+
+        float operations(string operation, string beforevalue, string aftervalue)
+        {
+            if (operation == "/")
+                return (float.Parse(beforevalue) / float.Parse(aftervalue));
+            if (operation == "^")
+                return (float)(Math.Pow(double.Parse(beforevalue), double.Parse(aftervalue)));
+            if (operation == "*")
+                return (float.Parse(beforevalue) * float.Parse(aftervalue));
+            if (operation == "+")
+                return (float.Parse(beforevalue) + float.Parse(aftervalue));
+            if (operation == "-")
+                return (float.Parse(beforevalue) - float.Parse(aftervalue));
+            return 0;
+        }
+    }
+}
